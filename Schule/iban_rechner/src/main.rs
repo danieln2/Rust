fc83beb610a6b578db26e7fe9f5fab52ccb4c11a
Daniel <Daniel>
2017-03-07 21:48:31 +0100

    added iban_rechner

---
+++ Schule/iban_rechner/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    let mut x: Vec<u16> = vec![0,0,4,1,3,1,0,9,8,7,6,5,4,3,2,1,0,0,1,0,9,0,0,7];
+    //let mut x: Vec<u16> = vec![0,0,4,1,3,1,0,0,6,0,7,7,5,7,0,0,0,0,0,0,9,3,1,5];
+   
+    let y = 97;
+    let mut return_string = String::new();
+    
+
+    let mut result = 0;
+    while x.len() >= 3 {
+        // nimmt die letzten drei Zahlen aus dem Vector und macht sie zu einer dreistelligen Zahl 
+        let buff = x[x.len()-1] * 100 + x[x.len()-2] * 10 + x[x.len()-3];
+
+        // nimmt die zusammengesetzte Zahl modul dem festgelegten, zweistelligen Wert
+        result = buff % y;
+
+        // entfernt die letzten drei Werte aus dem Vector
+        for i in 0..3
+        {
+            x.pop();
+        }
+
+        // schreibt den in der Modulo-Rechnung ermittelten Wert in umgekehrter Reihenfolge der Wertigkeit
+        // zurück in den Vector
+        x.push(result % 10);
+        x.push(result / 10);
+    }
+
+    let temp_ones: u16 = x[0]; 
+    let temp_tens: u16 = x[1]*10;
+    
+    result = 98 - (temp_ones + temp_tens);
+
+    if result / 10 == 0
+    {
+        return_string = "0".to_string() + &result.to_string();
+    }
+    else 
+    {
+        return_string = result.to_string();
+    }
+
+    println!("{:?}", return_string );
+
+}
